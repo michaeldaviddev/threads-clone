4bdcf463c3809ee7af6ffdeaa97cd0b397939aab
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-07-19 15:42:06 -0500

    Create tabs pages

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -7,30 +7,40 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('./home/home.page').then( m => m.HomePage)
       },
       {
-        path: 'tab2',
+        path: 'search',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('./search/search.page').then( m => m.SearchPage)
       },
       {
-        path: 'tab3',
+        path: 'new',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('./new/new.page').then( m => m.NewPage)
+      },
+      {
+        path: 'activity',
+        loadComponent: () =>
+          import('./activity/activity.page').then( m => m.ActivityPage)
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./profile/profile.page').then( m => m.ProfilePage)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/home',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/home',
     pathMatch: 'full',
   },
 ];
