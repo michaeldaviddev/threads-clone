d8960db30fa37d028fc6678babe8d410e8846eea
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-07-24 23:35:19 -0500

    Add thread actions

--- src/app/components/thread/thread.component.ts
+++ src/app/components/thread/thread.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-thread',
   templateUrl: './thread.component.html',
   styleUrls: ['./thread.component.scss'],
   standalone: true,
+  imports: [IonicModule]
 })
 export class ThreadComponent  implements OnInit {
 
