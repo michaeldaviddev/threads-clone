601ec0c1a1dc5b1716d030b187ac525809848c1a
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-08-09 23:09:52 -0500

    Add posts to thread component

--- src/app/components/thread/thread.component.ts
+++ src/app/components/thread/thread.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Post } from 'src/app/interfaces/post';
 
 @Component({
   selector: 'app-thread',
@@ -9,6 +10,7 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule]
 })
 export class ThreadComponent  implements OnInit {
+  @Input() post!: Post;
 
   constructor() { }
 
