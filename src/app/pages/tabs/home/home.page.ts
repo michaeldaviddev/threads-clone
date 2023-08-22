d06af12d4f943a36c9c66420d602c7fd3b1387ea
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-08-21 23:13:31 -0500

    Edit data

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -18,83 +18,83 @@ export class HomePage implements OnInit {
   posts: Post[] = [
     {
       user: {
-        username: 'user123',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/467.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/467.jpg',
+        username: 'user123'
       },
       caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      created_at: '1 h'
+      created_at: '1h'
     },
     {
       user: {
-        username: 'jane_doe',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/608.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/608.jpg',
+        username: 'jane_doe'
       },
       caption: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-      created_at: '2 h'
+      created_at: '2h'
     },
     {
       user: {
-        username: 'alex_smith',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/109.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/109.jpg',
+        username: 'alex_smith'
       },
       caption: 'Sed eget ipsum in libero facilisis tincidunt.',
-      created_at: '3 h'
+      created_at: '3h'
     },
     {
       user: {
-        username: 'user456',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/896.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/896.jpg',
+        username: 'user456'
       },
       caption: 'Nulla facilisi. Vivamus a varius justo.',
-      created_at: '4 h'
+      created_at: '4h'
     },
     {
       user: {
-        username: 'michael_johnson',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1128.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1128.jpg',
+        username: 'michael_johnson'
       },
       caption: 'Aenean tincidunt odio nec libero interdum, eu lacinia justo ultricies.',
-      created_at: '5 h'
+      created_at: '5h'
     },
     {
       user: {
-        username: 'user789',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/495.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/495.jpg',
+        username: 'user789'
       },
       caption: 'Etiam eu urna eget dolor malesuada luctus a eu tortor.',
-      created_at: '6 h'
+      created_at: '6h'
     },
     {
       user: {
-        username: 'emma_williams',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/704.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/704.jpg',
+        username: 'emma_williams'
       },
       caption: 'Suspendisse ut est at enim malesuada pharetra nec vel nisi.',
-      created_at: '7 h'
+      created_at: '7h'
     },
     {
       user: {
-        username: 'user101112',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/383.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/383.jpg',
+        username: 'user101112'
       },
       caption: 'Maecenas tincidunt dolor in lectus convallis, in suscipit mauris consequat.',
-      created_at: '8 h'
+      created_at: '8h'
     },
     {
       user: {
-        username: 'olivia_jones',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/513.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/513.jpg',
+        username: 'olivia_jones'
       },
       caption: 'Vestibulum ut orci at est mattis laoreet.',
-      created_at: '9 h'
+      created_at: '9h'
     },
     {
       user: {
-        username: 'user131415',
-        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/361.jpg'
+        avatar: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/361.jpg',
+        username: 'user131415'
       },
       caption: 'Nam feugiat libero eu metus gravida aliquet.',
-      created_at: '9 h'
+      created_at: '10h'
     }
   ];
 
